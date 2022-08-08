e89f395153c8fa930331dadac2293fa088420746
Earthyy <elardi2001@gmail.com>
2022-08-08 11:33:10 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// Import Section
+const express = require('express');
+
+// Initializations
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+
+
+
+
+
+
+
+// Server listening...
+app.listen(PORT,() => {
+    console.log('Server listening on PORT : ' + PORT);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
