0bf85f5ff6920a7710642288d449b228cae20dae
Earthyy <elardi2001@gmail.com>
2022-08-10 21:01:08 +0100

    minor fixes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const {envelopeRouter} = require('./src/routes/envelopes');
 const {transferRouter} = require('./src/routes/transfer');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 // Creating the express app
