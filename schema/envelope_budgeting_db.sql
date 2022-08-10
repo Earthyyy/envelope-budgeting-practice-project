c7f00ac805b698cd7d0c022151376e2787b99d67
Earthyy <elardi2001@gmail.com>
2022-08-10 23:21:05 +0100

    fixing schema

--- schema/envelope_budgeting_db.sql
+++ schema/envelope_budgeting_db.sql
@@ -18,4 +18,4 @@ CREATE TABLE "transactions" (
 );
 
 ALTER TABLE "envelopes" ADD CONSTRAINT "envelopes_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories" ("id") ON DELETE CASCADE;
-ALTER TABLE "transactions" ADD FOREIGN KEY ("envelope_id") REFERENCES "envelopes" ("id");
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_envelope_id_fkey" FOREIGN KEY ("envelope_id") REFERENCES "envelopes" ("id") ON DELETE CASCADE;
