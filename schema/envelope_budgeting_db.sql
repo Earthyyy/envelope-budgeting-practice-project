2b5b65bdac5a5e2481ebb9f5324e6e49454f268d
Earthyy <elardi2001@gmail.com>
2022-08-10 23:53:59 +0100

    adding mock data

--- schema/envelope_budgeting_db.sql
+++ schema/envelope_budgeting_db.sql
@@ -19,3 +19,17 @@ CREATE TABLE "transactions" (
 
 ALTER TABLE "envelopes" ADD CONSTRAINT "envelopes_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories" ("id") ON DELETE CASCADE;
 ALTER TABLE "transactions" ADD CONSTRAINT "transactions_envelope_id_fkey" FOREIGN KEY ("envelope_id") REFERENCES "envelopes" ("id") ON DELETE CASCADE;
+
+
+INSERT INTO categories(name) VALUES('savings');
+INSERT INTO categories(name) VALUES('groceries');
+INSERT INTO categories(name) VALUES('rent');
+
+INSERT INTO envelopes(category_id,balance) VALUES(1,90);
+INSERT INTO envelopes(category_id,balance) VALUES(2,35);
+INSERT INTO envelopes(category_id,balance) VALUES(3,175);
+
+INSERT INTO transactions(envelope_id,date,payment_amount,payment_receipient) VALUES(1,NOW(),15,'Uber');
+INSERT INTO transactions(envelope_id,date,payment_amount,payment_receipient) VALUES(1,NOW(),75,'Riot Games');
+INSERT INTO transactions(envelope_id,date,payment_amount,payment_receipient) VALUES(2,NOW(),30,'Street vendor');
+
